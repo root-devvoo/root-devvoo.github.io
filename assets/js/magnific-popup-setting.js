4775357cdf42155cdfd88b8e613d26e2fdfb6dc5
Brandon Jung <jwcnet93@gmail.com>
2022-01-08 12:34:09 +0900

    [2022-01-08] 이미지 사이 여백공간 없애기 도전 2

--- assets/js/magnific-popup-setting.js
+++ assets/js/magnific-popup-setting.js
@@ -14,7 +14,7 @@ $(document).ready(function() {
         });
 				
         // 2.3. p 태그 높이를 내부 컨텐츠 높이에 자동으로 맞추기.
-        $(this).parent('p').css('overflow', 'auto');
+        //$(this).parent('p').css('overflow', 'auto');
 				
         // 2.4. 이미지를 감쌀 태그 설정.
         return '<a href="' + $(this).attr('src') + '" style="width:' + $(this).attr('width') +'px;"><figure> </figure>' + '</a>';
